879a6c9f8e29ede5b312a407ae7053ae2223d421
Surendra Galwa <surendragalwa111@gmail.com>
2021-02-10 18:59:13 +0530

    Google search console tag added

--- pages/index.js
+++ pages/index.js
@@ -11,31 +11,32 @@ export default function Home() {
           name="description"
           content="Surendra Galwa's Portfolio"
         />
+        <meta name="google-site-verification" content="EmlYz-08tExjZMoRwNUu72m1ltuZeJx4Io7-iexB7kU" />
         <link rel='icon' href='/favs.ico' />
         <script type="application/ld+json">
           {
             "@context": "https://schema.org",
-          "@type": "VideoObject",
-          "name": "Introducing Ready Made Portfolio for Software Engineers",
-          "description": "This spring, Surendra is introducing the ready made portfolio template for all the developers.",
-          "thumbnailUrl": [
-            "https://i.picsum.photos/id/941/200/300.jpg",
-            "https://i.picsum.photos/id/941/200/300.jpg",
-            "https://i.picsum.photos/id/941/200/300.jpg"
-          ],
-          "uploadDate": "2016-03-31T08:00:00+08:00",
-          "duration": "PT1M54S",
-          "contentUrl": "https://www.youtube.com/watch?v=_mLTDwVQv1Y",
-          "embedUrl": "https://www.example.com/embed/123",
+        "@type": "VideoObject",
+        "name": "Introducing Ready Made Portfolio for Software Engineers",
+        "description": "This spring, Surendra is introducing the ready made portfolio template for all the developers.",
+        "thumbnailUrl": [
+          "https://i.picsum.photos/id/941/200/300.jpg",
+          "https://i.picsum.photos/id/941/200/300.jpg",
+          "https://i.picsum.photos/id/941/200/300.jpg"
+        ],
+        "uploadDate": "2016-03-31T08:00:00+08:00",
+        "duration": "PT1M54S",
+        "contentUrl": "https://www.youtube.com/watch?v=_mLTDwVQv1Y",
+        "embedUrl": "https://www.example.com/embed/123",
             "interactionStatistic": {
             "@type": "InteractionCounter",
               "interactionType": {
             "@type": "http://schema.org/WatchAction"
-        },
-      "userInteractionCount": 5647018
-    },
-    "regionsAllowed": "NL"
-  }
+      },
+    "userInteractionCount": 5647018
+  },
+  "regionsAllowed": "NL"
+}
         </script>
       </Head>
       <div className='page-container home-page'>
